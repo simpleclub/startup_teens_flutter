413a393139f0a8605e7b1df9821a760c5fed3d3c
Eric Dhom <eric.dhom@icloud.com>
2020-01-01 18:00:07 +0100

    hello flutter

---
+++ lib/hello_flutter.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: ToDo()));
+
+class ToDo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('To-Do-App'),
+        ),
+        body: Text('Hello Flutter')
+    );
+  }
+}
